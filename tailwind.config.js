5ff2f898dd630bef14ea171298af5c9ce7fc06be
ArsenIvanuh <arsenivanuh@gmail.com>
2025-03-24 12:43:39 -0700

    feat : add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    content: ["./*.html"], // Add any file patterns for your project
+    theme: {
+        extend: {
+            fontFamily: {
+                'bebas-neue': ['Bebas Neue', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
